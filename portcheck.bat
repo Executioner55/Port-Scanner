eb7ef2bd1c369ea35d7541b81f86316941e1ac31
Executioner <104730701+Executioner55@users.noreply.github.com>
2023-03-24 18:37:05 +0100

    Release V 1.0
    Command scan or type in any port you would like to scan

---
+++ portcheck.bat
@@ -0,0 +1,23 @@
+@echo off
+Title Port Checker by Executioner
+set /p Port=Port=
+IF "%Port%"=="" set /A Port=57614
+IF "%Port%"=="scan" set /A Port2=0 && cls && goto RapidScan:
+cls
+curl -s https://ipv4.am.i.mullvad.net/port/%Port% | findstr "false" >nul
+IF "%errorlevel%"=="0" echo [40;37mPort : [40;33m%Port% [40;37mis [40;31mClosed
+IF "%errorlevel%"=="1" echo [40;37mPort : [40;33m%Port% [40;37mis [40;32mOpen
+pause>nul
+exit
+:RapidScan
+cls
+set /p MaxPort=Max Port[Program end on Port reached]=
+IF "%MaxPort%"=="" goto RapidScan:
+cls
+:RapidScan2
+curl -s https://ipv4.am.i.mullvad.net/port/%Port2% | findstr "false" >nul
+IF "%errorlevel%"=="0" echo [40;37mPort : [40;33m%Port2% [40;37mis [40;31mClosed
+IF "%errorlevel%"=="1" echo [40;37mPort : [40;33m%Port2% [40;37mis [40;32mOpen
+set /A Port2=%Port2%+1
+IF "%Port2%"=="%MaxPort%" pause>nul
+goto RapidScan2:
